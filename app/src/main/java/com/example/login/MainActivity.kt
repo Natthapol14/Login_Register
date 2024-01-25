a4280708c22b35ba9c60ee73ffaa0eea31233643
Natthapol14 <you@example.com>
2024-01-25 14:07:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+    private lateinit var user: FirebaseUser
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+        button = findViewById(R.id.logout)
+        textView = findViewById(R.id.user_details)
+        user = auth.currentUser!!
+
+        if (user == null) {
+            val intent = Intent(applicationContext, Login::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            textView.text = user.email
+        }
+
+        button.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(applicationContext, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
